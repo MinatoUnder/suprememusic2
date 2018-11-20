ba78e311cc7db5196c6979eb263acb3fc14cff4e
MinatoUnder <44467517+MinatoUnder@users.noreply.github.com>
2018-11-20 22:07:14 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -357,8 +357,22 @@ client.user.setStatus("dnd")
 });
 
 
-client.on('ready',async () => {
-  client.channels.find(ch => ch.id === "514193447234830353" && ch.type === 'voice').join();
+  client.on('message', message => {
+  if (!message.guild) return;
+
+  if (message.content === 'join') {
+	   if (message.author.id !== '415595760990552065') return message.reply(':no_entry: | This Command Only For Minato :fire: !')
+ if(!message.author.id === '415595760990552065') return;
+    if (message.member.voiceChannel) {
+      message.member.voiceChannel.join()
+        .then(connection => { 
+          message.reply('لقد دخلت الروم بنجاح !');
+        })
+        .catch(console.log);
+    } else {
+      message.reply('يجب ان تكون في روم صوتي');
+    }
+  }
 });
 
 
